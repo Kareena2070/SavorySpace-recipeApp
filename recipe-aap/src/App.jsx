789f2537544cdfb54c088f524d55ff8015fb7b2d
Kareena2070 <kareenayadav1088@gmail.com>
2025-09-09 11:34:48 +0530

    remove separate Favorites component, use unique IDs, fix favorites toggle, add favorites filter

--- recipe-aap/src/App.jsx
+++ recipe-aap/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home'
 import Recipes from './pages/Recipes'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Favorities from './pages/favorite'
+// import Favorities from './pages/favorite'
 
 import "./pages/favorite.css"
 
@@ -19,12 +19,6 @@ function App() {
   const [showForm, setShowForm] = useState(false)
   const [isopen, setIsOpen] = useState(false)
   const [favorites, setFavorites] = useState([]);   // ✅ favorites state
-  
-  const toggleFavorite = (id) => {
-    setFavorites((prev) =>
-      prev.includes(id) ? prev.filter((f) => f !== id) : [...prev, id]
-    );
-  };
   
 
   return (
@@ -39,7 +33,6 @@ function App() {
         <ul className={`nav-links ${isopen? "show": ""}`}>
           <li><Link to={"/"} onClick={() => setIsOpen(false)}>Home</Link></li>
           <li><Link to={"/recipes"} onClick={() => setIsOpen(false)}  >Recipes</Link></li>
-          <li><Link to={"/favorites"} onClick={() => setIsOpen(false)}>Favorite</Link></li>
           <li><Link to={"/about"} onClick={() => setIsOpen(false)} >About</Link></li>
           <li><Link to={"/contact"} onClick={() => setIsOpen(false)}  >Contact</Link></li>
         </ul>
@@ -60,20 +53,9 @@ function App() {
         setShowForm={setShowForm}
         favorites={favorites}
         setFavorites={setFavorites}
-        toggleFavoritet={toggleFavorite}
         />}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
-        <Route
-        path='/favorites'
-        element={
-        <Favorities
-          recipes={recipesList}
-          favorites={favorites}
-          toggleFavorite={toggleFavorite}
-        />
-  }
-/>
 
       </Routes>
     </>
